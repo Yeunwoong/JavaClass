8e3494854db495ac4bdef519d94d9bab1d773ad3
Yeunwoong <60487315+Yeunwoong@users.noreply.github.com>
2020-09-22 07:13:49 +0900

    Update Reference.java

--- src/intro/Reference.java
+++ src/intro/Reference.java
@@ -1,9 +1,17 @@
 package intro;
 
 public class Reference {
+	public static void main(String[] args) {
+		SmartPhone phone = new SmartPhone();
+		phone.printLogo();
+		phone.sendCall();
+		phone.play();
+		System.out.println("3과 5를 더하면 " +
+			 phone.calculate(3,5));
+		phone.schedule();
 
+	}
 }
-
 interface PhoneInterface { // 인터페이스 선언
 	final int TIMEOUT = 10000; // 상수 필드 선언
 	void sendCall(); // 추상 메소드
@@ -12,24 +20,51 @@ interface PhoneInterface { // 인터페이스 선언
 		System.out.println("** Phone **");
 	}
 }
-
-
 interface MobilePhoneInterface extends PhoneInterface {
-	void sendSMS(); 		// 새로운 추상 메소드 추가
-	void receiveSMS(); 	// 새로운 추상 메소드 추가
-	default void printLogo() { // default 메소드
-		System.out.println("** MobilePhoneInterface **");
+	void sendSMS();
+	void receiveSMS();
+}
+interface MP3Interface { // 인터페이스 선언
+	public void play();
+	public void stop();
+}
+class PDA { // 클래스 작성
+	public int calculate(int x, int y) {
+		return x + y;
 	}
 }
 
-interface MP3Interface extends PhoneInterface{
-	void play(); // 추상 메소드
-	void stop(); // 추상 메소드
-	default void printLogo() { // default 메소드
-		System.out.println("** MobilePhoneInterface **");
+class SmartPhone extends PDA implements MobilePhoneInterface, MP3Interface {
+	// MobilePhoneInterface의 추상 메소드 구현
+	@Override
+	public void sendCall() {
+		System.out.println("따르릉따르릉~~");
+	}
+	@Override
+	public void receiveCall() {
+		System.out.println("전화 왔어요.");
+	}
+	@Override
+	public void sendSMS() {
+		System.out.println("문자갑니다.");
+	}
+	@Override
+	public void receiveSMS() {
+		System.out.println("문자왔어요.");
+	}
+	// MP3Interface의 추상 메소드 구현
+	@Override
+	public void play() {
+		System.out.println("음악 연주합니다.");
+	}
+	@Override
+	public void stop() {
+		System.out.println("음악 중단합니다.");
+	}
+	// 추가로 작성한 메소드
+	public void schedule() {
+		System.out.println("일정 관리합니다.");
 	}
 }
 
-interface MusicPhoneInterface extends MobilePhoneInterface, MP3Interface {
-	void playMP3RingTone(); // 새로운 추상 메소드 추가
-}
+
