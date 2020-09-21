f03a22761928637125b3c375c5cc0cbdb691c0e6
Yeunwoong <60487315+Yeunwoong@users.noreply.github.com>
2020-09-22 05:00:25 +0900

    Update Class_Three.java

--- src/intro/Class_Three.java
+++ src/intro/Class_Three.java
@@ -3,6 +3,8 @@ package intro;
 public class Class_Three {
 	public static void main(String[] args) {
 		C c = new C();
+		Person p = new Student("");		
+		p.draw();
 	}
 }
 
@@ -21,5 +23,29 @@ class B extends A {
 class C extends B {
 	public C() {
 		System.out.println("»ý¼ºÀÚC");
+	}	
+}
+
+class Person {
+	String name;
+	String id;
+	public Person(String name) {
+		this.name = name;
+	}
+	public void draw() {
+		System.out.println("Person draw");
+	}
+	
+}
+
+class Student extends Person {
+	String grade;
+	String department;
+	public Student(String name) {
+		super(name);
+	}
+	public void draw() {
+		System.out.println("Student draw");
 	}
 }
+
